bcc5e56abcabc4caf69a01b0459b402deb03605a
webdevelopment4everybody <56918351+webdevelopment4everybody@users.noreply.github.com>
2020-03-03 15:00:11 +0200

    kintamuju palyginimas

--- js/main.js
+++ js/main.js
@@ -49,6 +49,207 @@ const verte5 = h[4] + ', ' + h[3] + ', ' + h[2] + ', ' + h[1] + ', ' + h[0] + '.
 console.log(verte5);
 const verte6 = j[4] + ', ' + j[3] + ', ' + j[2] + ', ' + j[1] + ', ' + j[0] + '.';
 console.log(verte6);
+//KINTAMUJU PLAYGINIMAS
+//1.a
+if(a>b && a>c){
+    console.log("Pomidoras");
+}else{
+    console.log("Bandykite kita karta");
+}
+if(b>c && b>a){
+    console.log("Pomidoras");
+}else{
+    console.log("Bandykite kita karta");
+}
+if(c>a && c>b){
+    console.log("Pomidoras");
+}else{
+    console.log("Bandykite kita karta");
+}
+
+//1.b
+if(a<b && a<c){
+    console.log("Pomidoras");
+}else{
+    console.log("Bandykite kita karta");
+}
+if(b<a && b<c){
+    console.log("Pomidoras");
+}else{
+    console.log("Bandykite kita karta");
+}
+if(c<b && c<a){
+    console.log("Pomidoras");
+}else{
+    console.log("Bandykite kita karta");
+}
+//1.c
+if(a===b && b===c && c===a){
+    console.log("Pomidoras");
+}else{
+    console.log("Bandykite kita karta");
+}
+//1.d
+if(a !== b && b!==c && c!== a ){
+    console.log("Pomidoras");
+}else{
+    console.log("Bandykite kita karta");
+}
+//1.e
+if(a >= b){
+    console.log("Pomidoras");
+}else{
+    console.log("Bandykite kita karta");
+}
+if(a>=c){
+    console.log("Pomidoras");
+}else{
+    console.log("Bandykite kita karta");
+}
+if(b<=c){
+    console.log("Pomidoras");
+}else{
+    console.log("Bandykite kita karta");
+}
+if(b >= a){
+    console.log("Pomidoras");
+}else{
+    console.log("Bandykite kita karta");
+}
+if(c >= b){
+    console.log("Pomidoras");
+}else{
+    console.log("Bandykite kita karta");
+}
+if(c >= a){
+    console.log("Pomidoras");
+}else{
+    console.log("Bandykite kita karta");
+}
+//1.f
+if(a<=b){
+    console.log("Pomidoras");
+}else{
+    console.log("Bandykite kita karta");
+}
+if(a<=c){
+    console.log("Pomidoras");
+}else{
+    console.log("Bandykite kita karta");
+}
+if(b<=a){
+    console.log("Pomidoras");
+}else{
+    console.log("Bandykite kita karta");
+}
+if(b<=c){
+    console.log("Pomidoras");
+}else{
+    console.log("Bandykite kita karta");
+}
+if(c<=b){
+    console.log("Pomidoras");
+}else{
+    console.log("Bandykite kita karta");
+}
+if(c<=a){
+    console.log("Pomidoras");
+}else{
+    console.log("Bandykite kita karta");
+}
+//2.
+
+// const vardas = "Migle";
+// const zodis = "suo";
+// const maistas = "riesutai";
+const vardas_ilgis = vardas.length;
+console.log(vardas_ilgis);
+const zodis_ilgis = zodis.length;
+console.log(zodis_ilgis);
+const maistas_ilgis = maistas.length;
+console.log(maistas_ilgis);
+//3.a.
+
+if(vardas_ilgis > zodis_ilgis){
+    console.log(vardas);
+}else{
+    console.log(zodis); 
+}
+if(vardas_ilgis > maistas_ilgis){
+    console.log(vardas);
+}else{
+    console.log(maistas); 
+}
+if(maistas_ilgis > zodis_ilgis){
+    console.log(maistas);
+}else{
+    console.log(zodis); 
+}
+if(maistas_ilgis > vardas_ilgis){
+    console.log(maistas);
+}else{
+    console.log(vardas); 
+}
+if(zodis_ilgis > maistas_ilgis){
+    console.log(zodis);
+}else{
+    console.log(maistas); 
+}
+if(zodis_ilgis > vardas_ilgis){
+    console.log(zodis);
+}else{
+    console.log(vardas); 
+}
+//3.b
+if(vardas_ilgis < zodis_ilgis){
+    console.log(vardas);
+}else{
+    console.log(zodis); 
+}
+if(vardas_ilgis > maistas_ilgis){
+    console.log(vardas);
+}else{
+    console.log(zodis); 
+}
+//3.c ir 3.d
+if(vardas_ilgis === zodis_ilgis === maistas_ilgis){
+    console.log("Jie lygus")
+}else{
+    console.log("Jie nelygus");
+}
+//3.e ir f
+if(vardas_ilgis >= zodis_ilgis && vardas_ilgis >= maistas_ilgis){
+    console.log(true);
+}else{
+    console.log(false);
+}
+if(zodis_ilgis <= vardas_ilgis && zodis_ilgis <= maistas_ilgis){
+    console.log(true);
+}else{
+    console.log(false);
+}
+//4.
+// const d = [1,2,3,4,5];
+// console.log(d);
+// const e = [10,20,30,40,50];
+// console.log(e);
+// const f = [15,16,17,18,19,20];
+// console.log(f);
+
+let d_ilgis = d.length;
+console.log(d_ilgis);
+
+let e_ilgis = e.length;
+console.log(e_ilgis);
+//CIKLO FOR PANAUDOJIMAS
+//1.
+
+
+
+
+
+
+
 
 
 
