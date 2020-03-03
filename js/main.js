dc6f119d5037e553d5a85aac6156f73f9f489904
webdevelopment4everybody <56918351+webdevelopment4everybody@users.noreply.github.com>
2020-03-03 13:30:54 +0200

    veiksmai su kintamaisias done

--- js/main.js
+++ js/main.js
@@ -1,1 +1,34 @@
-console.log("hey");
+//VEIKSMAI SU KINTAMAISIAIS
+//1.
+const a = 1;
+console.log(a);
+const b = 2;
+console.log(b);
+const c = 3;
+console.log(c);
+//2.
+const vardas = "Migle";
+console.log(vardas);
+const zodis = "suo";
+console.log(zodis);
+const maistas = "riesutai";
+console.log(maistas);
+//3.
+const d = [1,2,3,4,5];
+console.log(d);
+const e = [10,20,30,40,50];
+console.log(e);
+const f = [15,16,17,18,19,20];
+console.log(f);
+//4.
+g = ["vienas", "du", "trys", "keturi", "penki"];
+console.log(g);
+h = ["vienas", "du", "trys", "keturi", "penki"];
+console.log(h);
+j = ["vienas", "du", "trys", "keturi", "penki"];
+console.log(j);
+
+
+
+
+
