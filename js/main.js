20418d84e85ef6600e1512b97504bd842c218dee
webdevelopment4everybody <56918351+webdevelopment4everybody@users.noreply.github.com>
2020-03-03 13:50:07 +0200

    veiksmai su kintamaisiais done

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-//VEIKSMAI SU KINTAMAISIAIS
+//KINTAMUJU INICIJAVIMAS
 //1.
 const a = 1;
 console.log(a);
@@ -23,10 +23,37 @@ console.log(f);
 //4.
 g = ["vienas", "du", "trys", "keturi", "penki"];
 console.log(g);
-h = ["vienas", "du", "trys", "keturi", "penki"];
+h = ["labas", "ate", "labas", "ate", "labas"];
 console.log(h);
-j = ["vienas", "du", "trys", "keturi", "penki"];
+j = ["avis", "arklys", "pele", "kate", "suo"];
 console.log(j);
+
+//VEIKSMAI SU KINTAMAISIAIS
+//1.
+const suma = a + b + c;
+console.log(suma);
+//2.
+const sujungti = vardas + ' ' + zodis + ' ' + maistas;
+console.log(sujungti);
+//3.
+const verte = d[0] - d[1] + d[2] - d[3] + d[4];
+console.log(verte);
+const verte2 = e[0] - e[1] + e[2] - e[3] + e[4];
+console.log(verte2);
+const verte3 = f[0] - f[1] + f[2] - f[3] + f[4];
+console.log(verte3);
+//4.
+const verte4 = g[4] + ', ' + g[3] + ', ' + g[2] + ', ' + g[1] + ', ' + g[0] + '.';
+console.log(verte4);
+const verte5 = h[4] + ', ' + h[3] + ', ' + h[2] + ', ' + h[1] + ', ' + h[0] + '.';
+console.log(verte5);
+const verte6 = j[4] + ', ' + j[3] + ', ' + j[2] + ', ' + j[1] + ', ' + j[0] + '.';
+console.log(verte6);
+
+
+
+
+
 
 
 
